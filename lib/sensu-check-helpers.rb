dd11824a278adc9c3b87341018f66d5ac1ec06ba
Ben Snape <ben.snape@itv.com>
2015-05-07 11:00:26 +0100

    added missing lib requires

--- lib/sensu-check-helpers.rb
+++ lib/sensu-check-helpers.rb
@@ -1,5 +1,7 @@
 require 'sensu-check-helpers/version'
 require 'sensu-check-helpers/alerts'
+require 'sensu-check-helpers/time'
+require 'sensu-check-helpers/request'
 
 require 'socket'
 require 'chronic_duration'
